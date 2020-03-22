f92cf2e0f3492f8808926fc228b77da7252a1398
Nawinav <avnaveenkumar16@gmail.com>
2020-03-22 09:06:37 +0530

    Add project files.

---
+++ MobileKart.Service/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileKart.Service
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+
+        }
+    }
+}
---
+++ MobileKart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobileKart.MvcApplication" Language="C#" %>
