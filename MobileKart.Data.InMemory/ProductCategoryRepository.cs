77ba84fc3fcbefc9f9ab65f9d3e2af79c60fe3d2
Nawinav <avnaveenkumar16@gmail.com>
2020-03-22 14:06:55 +0530

    Include properties in ProductCategory.cs (Id,Category)and then Create ProductCategoryManager Controller same as that of ProductManager by adding (Create,Index,Edit,Delete) based on the Method in (In.Memory library) of class ProductCategoryRepository.

---
+++ MobileKart.Data.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using MobileKart.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileKart.Data.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> products; //assigning model properties to products
+
+        public ProductCategoryRepository()
+        {
+            products = cache["productCategories"] as List<ProductCategory>;
+            if (products == null)
+            {
+                products = new List<ProductCategory>();//now only creating model properties 
+            }
+        }
+        public void commit()
+        {
+            cache["productCategories"] = products;
+        }
+        public void Insert(ProductCategory p)
+        {
+            products.Add(p);
+
+        }
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productToUpdate = products.Find(p => p.Id == productCategory.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+        public IQueryable<ProductCategory> Collection()
+        {
+            return products.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            ProductCategory productToDelete = products.Find(p => p.Id == Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+    }
+}
+
+
---
+++ MobileKart/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,103 @@
+﻿using MobileKart.Core.Models;
+using MobileKart.Data.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileKart.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        // GET: ProductCategoryManager
+        ProductCategoryRepository Context;
+        public ProductCategoryManagerController()
+        {
+            Context = new ProductCategoryRepository();
+        }
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = Context.Collection().ToList();
+            return View(productCategories);
+        }
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                Context.Insert(productCategory);
+                Context.commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategory = Context.Find(Id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Edit(string Id,ProductCategory productCategory)
+        {
+            ProductCategory productCategoryToUpdate = Context.Find(Id);
+            if (productCategoryToUpdate == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                productCategoryToUpdate.Category = productCategory.Category;
+                Context.commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory productCategory = Context.Find(Id);
+
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productCategoriesToDelete = Context.Find(Id);
+
+            if (productCategoriesToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Context.Delete(Id);
+                Context.commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
